2a53721f4735bef8fdf63ef6134c5a727c4f4801
Stefano Locati <stefano@obliquid.it>
2011-07-23 06:43:41 +0200

    Returning directly the data structure from ToDoList, just because
    MappingJacksonJsonView is present in servlet-context.xml it will be used

--- src/main/java/org/obliquid/restjson/web/ToDoList.java
+++ src/main/java/org/obliquid/restjson/web/ToDoList.java
@@ -6,21 +6,18 @@ import java.util.List;
 import org.obliquid.restjson.model.ToDoItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class ToDoList {
 
     @RequestMapping("/toDoList")
-    public ModelAndView test() {
+    public @ResponseBody List<ToDoItem> test() {
         List<ToDoItem> toDoList = new ArrayList<ToDoItem>();        
         toDoList.add(new ToDoItem(1, "First thing, first"));
         toDoList.add(new ToDoItem(1, "After that, do the second task"));
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("toDoList",  toDoList);
-        mav.setViewName("jsonView");
-        return mav;
+        return toDoList;
     }
     
 }
